a11eab804afdb7865d5fab03124abfeb48b73ad6
arunassc <20arunas@gmail.com>
2023-12-18 09:35:43 +0200

    Link css

--- views/categories/index.php
+++ views/categories/index.php
@@ -10,9 +10,9 @@ $categories = CategoryController::getAll();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="your-actual-integrity-hash" crossorigin="anonymous">
+    <link href="../../css/main.css" rel="stylesheet" integrity="your-integrity-hash" crossorigin="anonymous">
     <title>Computer</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="./main.css" rel="stylesheet" integrity="your-integrity-hash" crossorigin="anonymous">
 </head>
 
 <body>
